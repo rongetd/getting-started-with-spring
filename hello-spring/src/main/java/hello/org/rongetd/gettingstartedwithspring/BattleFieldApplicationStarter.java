fb4af65cf315e9b021bcb74cdf54577390fd5cd9
Pravin Ronge <rongejobs@gmail.com>
2017-02-12 21:23:16 +0530

    init

---
+++ hello-spring/src/main/java/hello/org/rongetd/gettingstartedwithspring/BattleFieldApplicationStarter.java
@@ -0,0 +1,12 @@
+package hello.org.rongetd.gettingstartedwithspring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class BattleFieldApplicationStarter {
+	public static void main(String[] args) {
+
+	}
+}
